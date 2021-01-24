c28c828a6a197a76de9bd2ff2c5d4fadfec4261d
roboticforest <roboticforest@gmail.com>
2021-01-23 21:56:02 -0800

    Created so projects don't have to include every custom function one-by-one.

---
+++ custom_config_file_helpers.cmake
@@ -0,0 +1,3 @@
+include(make_library_configs)   # Function for creating <libname>-config.cmake and <libname>-version.cmake files.
+include(make_version_header)    # Function for creating <libname>_version.h files.
+include(make_cmake_config_version_file)	# Funcion for creating <project-name>-config-version.cmake
